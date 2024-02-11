e422cada1b2938097199ac2a09bae30542d1e0b1
Nicolas <nico.daddabbo7100@gmail.com>
2024-02-11 11:08:16 +0100

    fix bug form array delete

--- src/app/components/demo/demo7/demo7.component.ts
+++ src/app/components/demo/demo7/demo7.component.ts
@@ -1,54 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-demo7',
   templateUrl: './demo7.component.html',
-  styleUrl: './demo7.component.scss'
+  styleUrl: './demo7.component.scss',
 })
 export class Demo7Component implements OnInit {
-
-  myGroup!: FormGroup
-  constructor(private _formBuilder : FormBuilder){
-
-  }
+  myGroup!: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
   ngOnInit(): void {
     this.myGroup = this._formBuilder.group({
-      "email": [null, [Validators.required, Validators.email]],
-      "password": [null, [Validators.required, this.monValidator()]],
-      "Pseudos" : this._formBuilder.array([
-
-      ])
-    })
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, this.monValidator()]],
+      Pseudos: this._formBuilder.array([
+        this._formBuilder.control('', Validators.required),
+      ]),
+    });
   }
 
-  getPseudos() : FormArray {
-    return this.myGroup.get("Pseudos") as FormArray
+  getPseudos(): FormArray {
+    return this.myGroup.get('Pseudos') as FormArray;
   }
 
-  addPseudo(){
-    this.getPseudos().push(new FormControl(null, [Validators.required]));
+  addPseudo() {
+    this.getPseudos().push(this._formBuilder.control(''));
   }
 
-  removePseudo(index : number){
-    this.getPseudos().removeAt(index)
+  removePseudo(index: number) {
+    this.getPseudos().removeAt(index);
   }
 
   handleSubmit() {
-   console.log(this.myGroup);
-   
-    
+    console.log(this.myGroup);
   }
 
-  monValidator() : ValidatorFn | null {
-    return (control : AbstractControl) : ValidationErrors | null  => {
-      
-        if(control.value != null && control.value.length < 5) {
-          return null
-        }
-        return {message: "invalid"}
-    }
+  monValidator(): ValidatorFn | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value != null && control.value.length < 5) {
+        return null;
+      }
+      return { message: 'invalid' };
+    };
   }
-
-
 }
